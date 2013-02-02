58152041dc448305126ae0a01a5c4cdde483bcd8
Andy Pellett <anrope@gmail.com>
2013-02-02 15:15:35 -0500

    start package.json

--- tweetgrabber.js
+++ tweetgrabber.js
@@ -1,1 +1,10 @@
 // grillinmybus
+
+var ts = require('twitter-stream');
+
+var stream = ts.connect({
+  screen_name: '',
+  password: '',
+  action: 'sample',
+  params: {locations: '-71.1832,42.3565,-71.1834,42.3567'},
+});
